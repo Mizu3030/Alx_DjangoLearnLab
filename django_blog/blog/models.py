7111c8ac8745717b4e0a07c44b0ee4792d03466b
dina <dinamah3030@gmail.com>
2025-12-07 00:13:57 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,29 @@
+# blog/models.py
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    tags = models.ManyToManyField("Tag", related_name="posts", blank=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -16,7 +39,20 @@ class Comment(models.Model):
         return f"Comment by {self.author} on {self.post}"
 
     def get_update_url(self):
-        return reverse("comment_update", kwargs={"post_pk": self.post.pk, "pk": self.pk})
+        return reverse("comment_update", kwargs={"pk": self.pk})
 
     def get_delete_url(self):
-        return reverse("comment_delete", kwargs={"post_pk": self.post.pk, "pk": self.pk})
+        return reverse("comment_delete", kwargs={"pk": self.pk})
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("tag_detail", kwargs={"tag_name": self.name})
