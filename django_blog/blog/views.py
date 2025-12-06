a96b68cf3e4a5ec7fe7ea113d9cce889d485a984
dina <dinamah3030@gmail.com>
2025-12-06 23:33:35 +0200

    Add CRUD views for blog posts and authentication updates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,18 +1,25 @@
 # blog/views.py
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from .forms import CustomUserCreationForm, ProfileForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm, ProfileForm, PostForm
+from .models import Post
+
+# -------------------------------
+# Authentication Views
+# -------------------------------
 
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("profile")
+            login(request, form.get_user())
+            return redirect("post_list")
     else:
         form = AuthenticationForm()
     return render(request, "auth/login.html", {"form": form})
@@ -20,18 +27,22 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("login")
-    # blog/views.py
+
 def register_view(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("profile")
+            return redirect("post_list")
     else:
         form = CustomUserCreationForm()
     return render(request, "auth/register.html", {"form": form})
-    # blog/views.py
+
+# -------------------------------
+#  Profile Management
+# -------------------------------
+
 @login_required
 def profile_view(request):
     if request.method == "POST":
@@ -43,4 +54,41 @@ def profile_view(request):
         form = ProfileForm(instance=request.user)
     return render(request, "auth/profile.html", {"form": form})
 
+# -------------------------------
+#  Blog Post CRUD Views
+# -------------------------------
 
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/post_list.html"
+    context_object_name = "posts"
+    paginate_by = 10
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/post_detail.html"
+    context_object_name = "post"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "posts/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class AuthorRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class PostUpdateView(LoginRequiredMixin, AuthorRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "posts/post_form.html"
+
+class PostDeleteView(LoginRequiredMixin, AuthorRequiredMixin, DeleteView):
+    model = Post
+    template_name = "posts/post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
