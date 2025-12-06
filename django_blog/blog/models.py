cbe67b783424bcfcb71be3d2ba2f45c9eb621ddd
dina <dinamah3030@gmail.com>
2025-12-06 23:56:36 +0200

    Add Comment model and update Post model methods

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,20 +1,22 @@
-# blog/models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ["created_at"]
 
     def __str__(self):
-        return self.title
+        return f"Comment by {self.author} on {self.post}"
 
-    def get_absolute_url(self):
-        return reverse("post_detail", kwargs={"pk": self.pk})
+    def get_update_url(self):
+        return reverse("comment_update", kwargs={"post_pk": self.post.pk, "pk": self.pk})
+
+    def get_delete_url(self):
+        return reverse("comment_delete", kwargs={"post_pk": self.post.pk, "pk": self.pk})
