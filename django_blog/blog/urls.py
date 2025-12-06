09a4dd3f27821b305afbc462835bf6ffa9399d92
dina <dinamah3030@gmail.com>
2025-12-07 00:18:24 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register_view, name="register"),
     path("profile/", views.profile_view, name="profile"),
 
- 
     path("post/", views.PostListView.as_view(), name="post_list"),
     path("post/new/", views.PostCreateView.as_view(), name="post_create"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
@@ -17,4 +17,7 @@ urlpatterns = [
     path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment_create"),
     path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment_update"),
     path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
+
+    path("tags/<str:tag_name>/", views.TagPostListView.as_view(), name="tag_detail"),
+    path("search/", views.PostSearchView.as_view(), name="post_search"),
 ]
