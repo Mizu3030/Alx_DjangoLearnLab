8acd87612a80825d3a4ddc0eb297454ec9a3b4c3
dina <dinamah3030@gmail.com>
2025-12-06 23:17:40 +0200

    Update urls.py

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -20,3 +20,13 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# blog/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register_view, name="register"),
+    path("profile/", views.profile_view, name="profile"),
+]
