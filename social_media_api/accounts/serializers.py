ab206e263f99497451378adacfeb93f504547947
dina <dinamah3030@gmail.com>
2025-12-21 18:41:40 +0200

    Refactor user serializers and update validation message

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,10 +1,29 @@
-# accounts/serializers.py
-from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "password2"]
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "كلمات المرور غير متطابقة"})
+        validate_password(attrs["password"])
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
 class UserSerializer(serializers.ModelSerializer):
     followers_count = serializers.IntegerField(read_only=True)
@@ -17,25 +36,3 @@ class UserSerializer(serializers.ModelSerializer):
             "followers_count", "following_count",
         ]
         read_only_fields = ["id", "followers_count", "following_count"]
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ["username", "email", "password", "password2"]
-
-    def validate(self, attrs):
-        if attrs["password"] != attrs["password2"]:
-            raise serializers.ValidationError({"password": "Passwords do not match"})
-        validate_password(attrs["password"])
-        return attrs
-
-    def create(self, validated_data):
-        validated_data.pop("password2")
-        password = validated_data.pop("password")
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
