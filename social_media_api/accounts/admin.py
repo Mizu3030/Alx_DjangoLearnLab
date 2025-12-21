92436203ff4ba57e3b6c91429392041cd757f142
dina <dinamah3030@gmail.com>
2025-12-21 18:46:48 +0200

    Enhance UserAdmin with additional fields and methods

--- social_media_api/accounts/admin.py
+++ social_media_api/accounts/admin.py
@@ -1,3 +1,37 @@
+# accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.html import format_html
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email", "bio", "profile_picture")}),
+        ("Relationships", {"fields": ("followers",)}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2"),
+        }),
+    )
+
+    list_display = (
+        "id", "username", "email", "first_name", "last_name",
+        "followers_count", "following_count", "avatar_preview", "is_staff",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("id",)
+    filter_horizontal = ("groups", "user_permissions", "followers")
+
+    def avatar_preview(self, obj):
+        if obj.profile_picture:
+            return format_html('<img src="{}" style="height:32px;width:32px;border-radius:50%;" />', obj.profile_picture.url)
+        return "-"
+    avatar_preview.short_description = "Avatar"
