5c67ebc78ebab03cff6d624cd5d0549782aec674
dina <dinamah3030@gmail.com>
2026-01-03 13:17:37 +0200

    Update urls.py

--- Alx_DjangoLearnLab/django_models/django_models/urls.py
+++ Alx_DjangoLearnLab/django_models/django_models/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # ربط التطبيق
+    path('', include('relationship_app.urls')),
 ]
-
 
